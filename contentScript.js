90f59008fd44c2d189027b6a59016df6cac44c63
Vo Minh Luan <61099649+black134614@users.noreply.github.com>
2024-04-06 23:51:20 +0700

    upoadcode
    up load code

---
+++ contentScript.js
@@ -0,0 +1,147 @@
+(() => {
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const { type, value } = obj;
+        if (type === "NEW") {
+            pageLoaded();
+        }
+    });
+    let stopProcessing = true;
+
+    // async function startAuto(wrapper, loop, watingTime) {
+    //     while (!stopProcessing) {
+    //         const button = document.querySelector('button.Crafting_craftingButton__Qd6Ke span');
+    //         if (button && !button.disabled) {
+    //             button.click();
+    //             const randomTime = getRandomInt(3, 7) * 200;
+    //             await sleep(watingTime + randomTime);
+    //             if (!stopProcessing) {
+    //                 button.click();
+    //                 await sleep(randomTime);
+    //                 if (!stopProcessing) {
+    //                     button.click();
+    //                 }
+    //             }
+    //         }
+    //         await sleep(1000)
+    //     }
+    // }
+    async function clickCraftingButton() {
+        const craftingButton = document.querySelector('.Crafting_craftingButton__Qd6Ke');
+        if (craftingButton && !craftingButton.disabled) {
+            craftingButton.click();
+        }
+    }
+
+    async function clickCloseCraftingButton() {
+        const craftingButton = document.querySelector('.Crafting_craftingCloseButton__ZbHQF');
+        if (craftingButton) {
+            craftingButton.click();
+        }
+    }
+
+    async function startAuto(wrapper, loop = 0, watingTime) {
+        let count = 0
+        if (!loop || loop === '' || loop === 0) {
+            while (!stopProcessing) {
+                const craftingButton = document.querySelector('.Crafting_craftingButton__Qd6Ke');
+                if (craftingButton && !craftingButton.disabled && craftingButton.textContent === "Create") {
+                    console.log("tao banh");
+                    await clickCraftingButton();
+                    ramdom_out = getRandomInt(4, 6) * 1000
+                    await sleep(ramdom_out);
+                    await clickCloseCraftingButton()
+                    console.log("doi ", watingTime - ramdom_out + "ms");
+                    await sleep(watingTime - ramdom_out);
+                    console.log("hoan thanh");
+                    count++
+                    document.title = `Đã chế tạo ${count} lần`;
+                }
+                if (craftingButton && !craftingButton.disabled && craftingButton.textContent === "Collect") {
+                    await clickCraftingButton()
+                }
+                await sleep(200);
+            }
+            stopProcessing = true;
+            wrapper.className = "toogle-btn auto-water auto-off";
+            wrapper.title = "Click to start auto";
+            document.title = "Đã chế tạo xong!" + loop + " Bánh";
+        }
+        else {
+            while (!stopProcessing && count < loop) {
+                const elements = document.querySelector(
+                    "button.Crafting_craftingButton__Qd6Ke span"
+                );
+                if (elements && !elements.disabled) {
+                    elements.click();
+                    const ramdomTime = getRandomInt(3, 7) * 200
+                    await sleep(watingTime + ramdomTime);
+                    elements.click();
+                    await sleep(ramdomTime);
+                    elements.click();
+                    count += 1
+                    document.title = "Đã chế tạo " + count + " lần";
+                }
+                await sleep(500)
+            }
+            stopProcessing = true;
+            wrapper.className = "toogle-btn auto-water auto-off";
+            wrapper.title = "Click to start auto";
+            document.title = "Đã chế tạo " + count + " lần";
+        }
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    function timeToMilliseconds(timeString) {
+        const [hours, minutes, seconds] = timeString.split(':').map(Number);
+        const totalMilliseconds = (hours * 3600 + minutes * 60 + seconds) * 1000;
+
+        return totalMilliseconds;
+    }
+
+    function stopAuto() {
+        stopProcessing = true;
+    }
+
+    function pageLoaded() {
+        const parent = document.getElementById('__next');
+        const wrapper = document.createElement('div');
+        wrapper.className = "toogle-btn auto-water";
+        parent.append(wrapper);
+
+        const inputNumber = document.createElement("input");
+        inputNumber.setAttribute("type", "number");
+        inputNumber.setAttribute("min", "1");
+        inputNumber.setAttribute("max", "999");
+        inputNumber.className = "auto-input-loop"
+        wrapper.append(inputNumber)
+
+        const bookmarkBtn = document.createElement("img");
+        bookmarkBtn.src = chrome.runtime.getURL("assets/cooking.png");
+        bookmarkBtn.title = "Click to start auto";
+        wrapper.append(bookmarkBtn);
+
+
+        bookmarkBtn.addEventListener('click', async (e) => {
+            if (!stopProcessing) {
+                wrapper.className = "toogle-btn auto-water auto-off";
+                wrapper.title = "Click to start auto";
+                stopAuto();
+            } else {
+                wrapper.className = "toogle-btn auto-water auto-on";
+                wrapper.title = "Click to stop auto";
+                const cratingWrapper = document.querySelectorAll("div.Crafting_craftingSection__QPjg4.Crafting_craftingColumn__v6ueF div")
+                const watingTime = timeToMilliseconds(cratingWrapper[7].textContent)
+                console.log("nau mat: ", watingTime)
+                stopProcessing = false;
+                await startAuto(wrapper, inputNumber.value || 0, watingTime);
+            }
+        });
+    }
+    pageLoaded();
+})();
---
+++ popup.js
@@ -0,0 +1,12 @@
+// adding a new bookmark row to the popup
+const pageLoaded = () => {};
+
+const viewBookmarks = () => {};
+
+const onPlay = e => {};
+
+const onDelete = e => {};
+
+const setBookmarkAttributes =  () => {};
+
+document.addEventListener("DOMContentLoaded", () => {});
